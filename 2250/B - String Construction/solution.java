d714a5db26cc3ce5774ce04b865cf1c429a0324c
Rishav Kumar <rk2845897@gmail.com>
2026-07-27 23:34:08 +0530

    Add String Construction [B] from Codeforces

--- 2250/B - String Construction/solution.java
+++ 2250/B - String Construction/solution.java
@@ -5,44 +5,53 @@ import java.util.StringTokenizer;
  
 public class StringConstructor {
     public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int t=Integer.parseInt(br.readLine().trim());
-        while (t-->0){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        StringBuilder result = new StringBuilder();
+ 
+        while (t-- > 0) {
             StringTokenizer in = new StringTokenizer(br.readLine());
-            int n=Integer.parseInt(in.nextToken());
-            int k=Integer.parseInt(in.nextToken());
-            StringBuilder sb=new StringBuilder();
-            if(k>n-2){
-                System.out.println(-1);
+            int n = Integer.parseInt(in.nextToken());
+            int k = Integer.parseInt(in.nextToken());
+ 
+            StringBuilder sb = new StringBuilder();
+            if (k > n - 2) {
+                result.append(-1).append('
+');
                 continue;
             }
-            int numOf1=(k+1)/2+1;
-            int numOf0=(k/2)+1;
-            int i=0;
-            if(k<(n-2)){
-                i=1;
+ 
+            int numOf1 = (k + 1) / 2 + 1;
+            int numOf0 = (k / 2) + 1;
+            int i = 0;
+ 
+            if (k < (n - 2)) {
+                i = 1;
                 sb.append(0);
             }
-            while(numOf1-->0 && i<n) {
+            while (numOf1-- > 0 && i < n) {
                 i++;
                 sb.append(1);
             }
-            while(numOf0-->0 && i<n) {
+            while (numOf0-- > 0 && i < n) {
                 i++;
                 sb.append(0);
             }
-            int num=1;
-            while (i<n){
+            int num = 1;
+            while (i < n) {
                 i++;
-                if(num==1){
+                if (num == 1) {
                     sb.append(1);
-                    num=0;
-                }else{
+                    num = 0;
+                } else {
                     sb.append(0);
-                    num=1;
+                    num = 1;
                 }
             }
-            System.out.println(sb.toString());
+            result.append(sb).append('
+');
         }
+ 
+        System.out.print(result);
     }
 }
