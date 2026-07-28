4c841f44acc8d94ba85d0cdd7b4dc5ebc6810328
Rishav Kumar <rk2845897@gmail.com>
2026-07-28 23:25:42 +0530

    Add An Alternative Way [D] from Codeforces

--- 2241/D - An Alternative Way/solution.java
+++ 2241/D - An Alternative Way/solution.java
@@ -1,29 +1,62 @@
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.Scanner;
+//import java.util.StringTokenizer;
+//
+//public class AnAlternativeWay {
+//    public static void main(String[] args) throws IOException {
+//        Scanner in=new Scanner(System.in);
+//        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+//        int t=in.nextInt();
+//        while (t-->0){
+//            int n=in.nextInt();
+//            long[] a=new long[n];
+//            for(int i=0;i<n ;i++){
+//                a[i]=in.nextLong();
+//            }
+//            long[] b=new long[n];
+//            for(int i=0;i<n;i++){
+//                b[i]=in.nextLong();
+//            }
+//            for(int i = 1; i < n; i++) a[i] += a[i - 1];
+//            for(int i = 1; i < n; i++) b[i] += b[i - 1];
+//            boolean possible=true;
+//            for(int i=0;i<n;i++){
+//                if(a[i]>b[i]){
+//                    possible=false;
+//                    break;
+//                }
+//            }
+//            if(possible) System.out.println("YES");
+//            else System.out.println("NO");
+//        }
+//    }
+//}
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.StringTokenizer;
  
 public class AnAlternativeWay {
     public static void main(String[] args) throws IOException {
         Scanner in=new Scanner(System.in);
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         int t=in.nextInt();
         while (t-->0){
             int n=in.nextInt();
-            long[] a=new long[n];
+            int[] a=new int[n];
             for(int i=0;i<n ;i++){
-                a[i]=in.nextLong();
+                a[i]=in.nextInt();
             }
-            long[] b=new long[n];
+            int[] b=new int[n];
             for(int i=0;i<n;i++){
-                b[i]=in.nextLong();
+                b[i]=in.nextInt();
             }
-            for(int i = 1; i < n; i++) a[i] += a[i - 1];
-            for(int i = 1; i < n; i++) b[i] += b[i - 1];
             boolean possible=true;
+            long v=0;
             for(int i=0;i<n;i++){
-                if(a[i]>b[i]){
+                v+=(a[i]-b[i]);
+                if(v>0) {
                     possible=false;
                     break;
                 }
